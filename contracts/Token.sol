843c7d14a636985f470eeee7669f65202d6f3101
KissConsult <56354937+KissConsult@users.noreply.github.com>
2020-02-25 19:58:37 +0100

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,20 +1,9 @@
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.0;
 
-import "../client/node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
 
 contract Token is ERC20Detailed{
 
-    string public name;
-    string public  symbol;
-    uint8 public  decimals;
-constructor(
-    string memory _name,
-    string memory _symbol,
-    uint8 _decimals
-)public {
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
-}
+    
 }
