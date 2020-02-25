110b4cc28dc0d99cc40dec052492ebdc081c3ed4
KissConsult <56354937+KissConsult@users.noreply.github.com>
2020-02-25 19:24:22 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var testContract = artifacts.require("./testContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(testContract);
 };
