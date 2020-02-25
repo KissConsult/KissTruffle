2598b8ac61a83df166b1322a29b8db7a107a936d
KissConsult <56354937+KissConsult@users.noreply.github.com>
2020-02-25 19:23:18 +0100

    Create testContract.js

---
+++ client/src/components/testContract.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import testContractJson from "../contracts/testContract.json";
+
+
+class testContract  extends Component {
+  
+  
+    
+    state = {
+      };
+    
+    
+  
+     render(){
+      
+       return (
+         
+        <div> <h2>TEST</h2> </div>
+       )
+
+       
+     } 
+  
+  
+    
+    }
+  
+
+
+export default testContract;
