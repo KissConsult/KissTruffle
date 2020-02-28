0226d222e8debb5b935cfd516859c2b761c38ea7
KissConsult <56354937+KissConsult@users.noreply.github.com>
2020-02-28 15:43:33 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var testContract = artifacts.require("./testContract.sol");
-var Token = artifacts.require("./Token.sol");
+var Token20 = artifacts.require("./Token20.sol");
+var Token721 = artifacts.require("./Token721.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(testContract);
-  deployer.deploy(Token,"Name","TST",18)
+  deployer.deploy(Token20,"KissConsult","KISS",18);
+  deployer.deploy(Token721,"KissConsult","KISS")
 };
